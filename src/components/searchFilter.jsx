21c1ad09aa5f65f262a92d9bbc0655275f516cb2
LahiruRJ <sanlahiru720@gmail.com>
2022-01-18 21:46:56 +0530

    Implement search

--- src/components/searchFilter.jsx
+++ src/components/searchFilter.jsx
@@ -1,9 +1,21 @@
 import Mockdata from '../MOCK_DATA.json';
+import { useState } from "react";
+
+
 const SearchTable = () => {
+    const [searchTerm, setsearchTerm] = useState("");
     return ( 
-        <div>
-<table>
-    <thead>
+        <div className="container">
+            <input
+            type="text"
+            placeholder="Search.." className="form-control" style={{marginTop:50, marginBottom:20, width:"40%"}}
+            
+            onChange={(e)=>{
+              setsearchTerm(e.target.value);  
+            }}/>
+            
+<table className="table table-bordered">
+    <thead className="thead-dark">
         <tr>
             <th>First Name</th>
             <th>Last Name</th>
@@ -13,7 +25,15 @@ const SearchTable = () => {
         </tr>
     </thead>
     <tbody>
-        {Mockdata.map((m)=>(
+        {Mockdata.filter(val=>{
+            if(searchTerm ===''){
+                return val;
+            }else if(val.first_name.toLowerCase().includes(searchTerm.toLowerCase()) || 
+            val.last_name.toLowerCase().includes(searchTerm.toLowerCase()) )
+            {
+                return val;
+            }
+        }).map((m)=>(
             <tr key={m.id}>
                 <td>{m.first_name}</td>
                 <td>{m.last_name}</td>
